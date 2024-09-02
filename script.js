f3b17aad90413c09339ed081612ee998de1bed2d
Mavelasse Zacarias <mrvllscbl41@gmail.com>
2024-09-02 19:27:43 +0200

    Add GameController factory

--- script.js
+++ script.js
@@ -15,18 +15,82 @@ function  Gameboard() {
         console.log(board);
     }
 
-    const placeToken = (x, y, playerToken) => {
-        if (board[x][y] != EMPTY) {
-            console.log("Invalid move!");
+    const getBoard = () => board;
+
+    const placeToken = (r, c, playerToken) => {
+        if (board[r][c] != EMPTY) {
+            console.log("Invalid Move!");
+            return 0;
         }
         else {
-            board[x][y] = playerToken;
+            board[r][c] = playerToken;
         }
+        return 1;
     }
-    return {printBoard, placeToken};
+    return {printBoard, getBoard, placeToken};
 }
 
-function GameController() {
-}
+function GameController(playerOneName="PlayerOne",
+    playerTwoName="Computer") {
+    
+    const SIZE = 3;
 
-let game = Gameboard();
+    const board = Gameboard();
+
+    const players = [{name:playerOneName, token: "X"},
+                    {name:playerTwoName, token: "O"}];
+
+    let activePlayer = players[0];
+
+    const switchActivePlayer = ()=> {
+        activePlayer = activePlayer.name === players[0].name ? players[1]: players[0];
+    }
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${activePlayer.name}'s turn`)
+    }
+
+    const checkWinner = (gameBoard)=> {
+
+        // Check Filled board
+
+        // Check Rows
+        for (let i = 0; i < SIZE; i++) {
+            row = gameBoard[i];
+            if (row[0] === row[1] && row[1] === row[2]) {
+                if (row[0] === players[0].token) {
+                    console.log("You Win!")
+                }
+                else if (row[0] === players[1].token) {
+                    console.log("Computer Wins")
+                }
+                return 1;
+            }
+        }
+        // Check Columns
+
+        return 0;
+        // Check Diagonals
+    }
+
+    const playRound = (row, column) => {
+        let switchPlayer;
+        if (activePlayer === players[0]) {
+            switchPlayer = board.placeToken(row, column, activePlayer.token)
+        }
+        else {
+
+            let randRow = Math.floor(Math.random() * 3);
+            let randCol = Math.floor(Math.random() * 3);
+            switchPlayer = board.placeToken(randRow, randCol, activePlayer.token)
+        }
+        if (switchPlayer) {
+            switchActivePlayer();
+        }
+        printNewRound();
+    }
+    printNewRound();
+
+    return {playRound};
+}
